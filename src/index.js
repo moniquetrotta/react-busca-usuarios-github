d9139f5dd1c171b752a3de93d6fb21e0bc0bfd3a
moniquetrotta <monique.trotta@gmail.com>
2021-07-08 20:36:48 -0300

    primeiro commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Repositories from './pages/Repositories';
+//import './index.css';
+//import * as serviceWorker from './serviceWorker';
+//import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  //<Repositories />,
+  <App title="Hello Gama Academay" user="Gama Academy"/>,
+  document.getElementById('root')
+);
+
+//serviceWorker.unregister();
